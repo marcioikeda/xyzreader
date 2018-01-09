5faebb1067d400cb9593ad6c45b00e7f9a25d4af
marcio.ikeda <marcioikeda770@gmail.com>
2018-01-09 19:03:25 -0200

    Detail Activity

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -9,10 +9,13 @@ public class Config {
     public static final URL BASE_URL;
     private static String TAG = Config.class.toString();
 
+    public static final String URL_UDACITY = "https://go.udacity.com/xyz-reader-json";
+    public static final String URL_ALTERNATIVE = "https://raw.githubusercontent.com/TNTest/xyzreader/master/data.json";
+
     static {
         URL url = null;
         try {
-            url = new URL("https://go.udacity.com/xyz-reader-json" );
+            url = new URL(URL_ALTERNATIVE );
         } catch (MalformedURLException ignored) {
             // TODO: throw a real error
             Log.e(TAG, "Please check your internet connection.");
